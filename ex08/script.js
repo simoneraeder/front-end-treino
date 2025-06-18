d468a302906a2cbe9173e3d4763b8f55c133523f
Simone <simoneraeder@live.com>
2025-06-18 20:35:43 -0300

    js

--- ex08/script.js
+++ ex08/script.js
@@ -1,1 +1,30 @@
-console.log("Olá, mundo!")
+//Selecionar elementos
+
+const input = document.getElementById('tarefaInput');
+const btn = document.getElementById ('adicionarBtn');
+const lista = document.getElementById("listaTarefas");
+
+btn.addEventListener('click', function () {
+    const texto = input.value.trim()
+  if(texto === ""){
+    alert("Digitar uma tarefa!");
+    return
+  }
+
+  // Criar um novo <li> com o value -> texto
+  const item = document.createElement('li');
+  item.textContent = texto;
+
+  const btnRemover = document.createElement('button');
+  btnRemover.textContent = "Remover";
+  btnRemover.style.marginLeft = '8px';
+  btnRemover.addEventListener('click', function () {
+    lista.removeChild(item)
+  })
+
+  item.appendChild(btnRemover);
+  lista.appendChild(item);
+  input.value = '';
+
+
+})
